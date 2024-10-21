b92e04c14b7c19a456e93e5d4f42584d41fd8674
hudson <hudsonpksj@gmail.com>
2024-10-21 10:17:41 -0300

    instalando sqlalchemy

--- aprendendo.py
+++ aprendendo.py
@@ -1,2 +1,8 @@
 # python aprendendo.py
-print("aprendendo")
+from sqlalchemy import create_engine 
+
+#conectar ao SQlite em memoria
+
+engine = create_engine('sqlite://meubanco.db', echo=True)
+
+print("conexao com SQLite estabelecida. ")
