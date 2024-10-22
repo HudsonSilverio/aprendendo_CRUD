7ccc3aa406c6fd3f114ca3e031d93b54c9b6731b
hudson <hudsonpksj@gmail.com>
2024-10-22 19:12:58 -0300

    criando estrutura

--- aprendendo.py
+++ aprendendo.py
@@ -1,8 +1,50 @@
 # python aprendendo.py
 from sqlalchemy import create_engine 
 
-#conectar ao SQlite em memoria
+# 01. conectar ao SQlite em memoria
 
-engine = create_engine('sqlite://meubanco.db', echo=True)
+engine = create_engine('sqlite:///meubanco.db', echo=True)
 
 print("conexao com SQLite estabelecida. ")
+
+# 02. Codigos para abstrair o bando de dados para os objetos em py
+from sqlalchemy.orm import declarative_base 
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+# 03. Criando a tabela 
+
+class Usuario(Base):
+    __tablename__ = "usuarios" # nome da tabela
+
+# Cada linha desse codigo sera uma COLUNA na tabela
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    idade = Column(Integer)
+
+# comando para criar as tabelas
+
+Base.metadata.create_all(engine)
+
+# 04. Adicionando informacoes dentro do banco de dados:
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+novo_usuario = Usuario(nome="debora", idade=29)
+session.add(novo_usuario)
+session.commit()
+
+print("Usuario inserido com sucesso !")
+#with Session () as session:
+    #novo_usuario = Usuario(nome="Ana", idade=25)
+    #session.add(novo_usuario)
+
+
+### codigo usado para realizar querys dentro BD usando PY
+
+#usuario = session.query(Usuario).filter_by(nome="João").first()
+#print(f"Usuario encontrado: {usuario.nome}, Idade: {usuario.idade}")
